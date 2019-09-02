f2141034a380b6228ec5f554a3f137df4f02dffd
Melissa Christman <melchrstmn@gmail.com>
2019-09-02 02:37:01 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  puts num1 + num2
+  6 + 3
 end
 
 def subtraction(num1, num2)
-
+  puts num1 - num2
+  10 - 5
 end
 
 def division(num1, num2)
-
+  puts num1 / num2
+  50 / 2
 end
 
 def multiplication(num1, num2)
-
+  puts num1 * num2
+  60 * 2
 end
 
 def modulo(num1, num2)
-
+  puts num1 % num2
+  10 % 6
 end
 
 def square_root(num)
-
+  Math.sqrt(81)
 end
